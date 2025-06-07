f087655e758cd9a15e8c0b88e7308488d31a2b97
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2025-06-07 18:15:21 +0530

    feat(auth): implement user login route with JWT support

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const { z } = require("zod");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const userRouter = express.Router();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const { UserModel, PurchasesModel } = require("../db");
 
@@ -33,17 +39,54 @@ userRouter.post("/signup", userAuth, async (req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
 
-  await UserModel.create({
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await UserModel.create({
+      email: email,
+      password: hashedPassword,
+      firstName: firstName,
+      lastName: lastName,
+    });
+  } catch (e) {
+    return res.status(400).json(e);
+  }
+
+  res.status(200).json("User sucessfully signed up");
+}); //user signup route completed
+
+userRouter.post("/login", userAuth, async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+
+  const user = await UserModel.findOne({
     email: email,
-    password: password,
     firstName: firstName,
     lastName: lastName,
   });
 
-  res.status(200).json("User sucessfully signed up");
-});
+  if (!user)
+    return res.status(400).json("User with given email or name not found");
 
-userRouter.post("/login", (req, res) => {});
+  console.log(user);
+
+  const isPasswordTrue = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordTrue)
+    return res.status(400).json("Incorrect password - please try again");
+
+  const userId = user._id.toString();
+
+  //JWT logic
+  const token = jwt.sign({ id: userId }, JWT_SECRET);
+
+  res.status(200).json({
+    message: "User has scuccessfully logged in",
+    token: token,
+  });
+}); //User login route completed
 
 userRouter.get("/purchases", (req, res) => {
   res.json("No records to show");
