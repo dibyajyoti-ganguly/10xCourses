9791e1519056c2fc686bbe58ca937fbc76be8ab7
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2025-06-07 14:33:47 +0530

    Add middleware for user and admin input validations using Zod

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const { z } = require("zod");
 const adminRouter = express.Router();
 
 const { AdminModel, CourseModel } = require("../db");
 
-adminRouter.post("/signup", (req, res) => {});
+adminRouter.use(express.json());
+
+//Middleware for admin auth
+const adminAuth = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password)
+    return res.status(400).json("400 Bad Request - missing field(s)");
+
+  const User = z.object({
+    email: z.string().min(3).max(100).email(),
+    password: z.string().min(3).max(100),
+  });
+
+  const parsedDatawithSuccess = User.safeParse({ email, password });
+
+  if (!parsedDatawithSuccess.success)
+    return res.status(400).json(parsedDatawithSuccess.error.issues[0].message);
+
+  next();
+};
+
+adminRouter.post("/signup", adminAuth, (req, res) => {});
 
 adminRouter.post("/login", (req, res) => {});
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const { z } = require("zod");
 const userRouter = express.Router();
 
 const { UserModel, PurchasesModel } = require("../db");
 
 userRouter.use(express.json());
 
-userRouter.post("/signup", async (req, res) => {
+//Middleware for user auth
+const userAuth = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password)
+    return res.status(400).json("400 Bad Request - missing field(s)");
+
+  const User = z.object({
+    email: z.string().min(3).max(100).email(),
+    password: z.string().min(3).max(100),
+  });
+
+  const parsedDatawithSuccess = User.safeParse({ email, password });
+
+  if (!parsedDatawithSuccess.success)
+    return res.status(400).json(parsedDatawithSuccess.error.issues[0].message);
+
+  next();
+};
+
+userRouter.post("/signup", userAuth, async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const firstName = req.body.firstName;
